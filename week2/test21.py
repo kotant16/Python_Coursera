57a17e20a43252dde4640a34abfb3abdcaa15ecd
antonkotov <anton.kotov@gmail.com>
2019-06-29 11:38:34 +0300

    code is wrong for test in Coursera
    Signed-off-by: antonkotov <anton.kotov@gmail.com>

--- week2/test21.py
+++ week2/test21.py
@@ -5,7 +5,7 @@ d = int(input())
 
 # def solver(a, b, c, d):
 A = (a == 0 & b == 0)  # first line in 0
-# B = a!=0 # first line not parallel
+B = a!=0 # first line not parallel
 C = (a == 0 & b != 0)  # first line parallel
 D = (c == 0 & d == 0)  # second line in zero
 E = (b / a == d / c)  # both lines intersects in one point

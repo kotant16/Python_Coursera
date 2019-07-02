716e6528d3777ced60e87356e8ec92c3e0a4a392
RUKotovA <anton.kotov@gmail.com>
2019-07-02 11:03:34 +0300

    new task added
    Signed-off-by: RUKotovA <anton.kotov@gmail.com>

--- week2/test22.py
+++ week2/test22.py
@@ -1,12 +1,12 @@
-k = input()
-m = input()
-n = input()
+k = int(input())
+m = int(input())
+n = int(input())
 
 if min(k, m, n) <= 0:
     print(0)
-sides = n*2
-cycles = sides//k
-if sides%n == 0:
+sides = n * 2
+cycles = sides // k
+if sides % n == 0:
     print(cycles*m)
 else:
     print(cycles*m + m)

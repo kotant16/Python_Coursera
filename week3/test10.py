0090d53a16d65fce6dc1b68bfb8c3a2ea59b9479
RUKotovA <anton.kotov@gmail.com>
2019-07-18 12:28:16 +0300

    new task added
    Signed-off-by: RUKotovA <anton.kotov@gmail.com>

--- week3/test10.py
+++ week3/test10.py
@@ -8,9 +8,11 @@ if d >= eps:
     x1 = (-b - math.sqrt(d)) / 2 / a
     x2 = (-b + math.sqrt(d)) / 2 / a
     if x1 > x2:
-        print(x2, x1)
+        print(2, x2, x1)
     elif x1 < x2:
-        print(x1, x2)
+        print(2, x1, x2)
+    else:
+        print(3)
 elif abs(d) < eps:
     x1 = -b / 2 / a
     print(1, x1)
